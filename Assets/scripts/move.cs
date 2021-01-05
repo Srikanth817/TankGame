746297755fd00eecff0192c2094582ec90da6a42
Srikanth <srikanth817@gmail.com>
2021-01-04 17:33:00 -0700

    test
    a

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float moveSpeed;
+    public float moveSpeed2;
+    public Rigidbody2D rb;
+    //public Camera cam;
+
+    float dx;
+    float dy;
+
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dx = Input.GetAxisRaw("Horizontal");
+        dy = Input.GetAxisRaw("Vertical");
+        //MousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+    }
+
+    private void FixedUpdate(){
+        float angleV =-dx*moveSpeed;
+        float velCom = dy*moveSpeed2;
+        float xv = (float) Math.Cos(rb.rotation*(Math.PI/180));
+        float yv = (float) Math.Sin(rb.rotation*(Math.PI/180));
+        Vector2 movement = new Vector2(xv*dy*moveSpeed2,yv*dy*moveSpeed2);
+
+        rb.angularVelocity = angleV;
+        rb.velocity = movement;
+
+       
+        //Debug.Log("Angle: " + rb.rotation);
+    }
+}
---
+++ Assets/turret.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turret : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+public int rotationOffset = 0;
+//public Transform transform;
+        Vector3 MousePos;
+    // Update is called once per frame
+    void Update()
+    {
+        MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //MousePos.Normalize();
+        if(Input.GetButtonDown("Fire1")){
+            Shoot(); 
+        }
+        
+    }
+    private void FixedUpdate(){
+        Vector2 lookDir = MousePos-transform.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x)*Mathf.Rad2Deg ;
+        transform.rotation =  Quaternion.Euler (0f, 0f, angle);
+        //transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler (0f, 0f, angle + rotationOffset), 200 * Time.deltaTime);
+     }
+    void Shoot(){
+
+    }
+}
